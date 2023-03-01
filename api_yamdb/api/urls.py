34dd4b6cdeb6d8c09b9d840da7aa8e29c4c544e6
MihailovAlexander <1c934567@gmail.com>
2023-03-01 15:31:35 +0700

    new urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import UserViewSet
+from api.views import CommentViewSet, UserViewSet, ReviewViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
-
+router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments/(?P<comment_id>\d+)', CommentViewSet)
+router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)', ReviewViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
