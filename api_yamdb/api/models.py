6fb8c85cd97daec611c407d2bba46c127a5f6c1a
SleekVortex <andretyklclash@gmail.com>
2023-03-03 12:21:30 +0700

    Добавил модель titleÑ

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .validators import validate_year
 
 
 class Genre(models.Model):
@@ -9,3 +10,30 @@ class Genre(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=256)
     slug = models.SlugField(max_length=50, unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название'
+    )
+    year = models.IntegerField(
+        validators=[validate_year],
+        verbose_name='Год создания'
+    )
+    description = models.CharField(
+        max_length=256,
+        null=True,
+        verbose_name='Описание'
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        related_name='genres',
+        verbose_name='Жанры произведения',
+    )
+    category = models.ForeignKey(
+        Category,
+        related_name='category',
+        verbose_name='Категория произведения',
+        on_delete=models.CASCADE,
+    )
